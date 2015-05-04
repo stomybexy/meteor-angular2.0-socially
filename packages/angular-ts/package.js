f00430d13a9b83d736fd72e754321f19c1499f2e
Netanel Gilad <netanelgilad@gmail.com>
2015-05-04 11:56:35 +0300

    clean angular2-typescript package

--- packages/angular-ts/package.js
+++ packages/angular-ts/package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'angular-ts',
+  name: 'netanelgilad:angular2-typescript',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Compile Angular2 code using Typescript compiler.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'http://github.com/netanelgilad/meteor-angular2-typescript',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name : 'ts',
   sources : [
-    'plugin/handler.js',
+    'plugin/handler.js'
   ],
   npmDependencies : {
     'typescript' : '1.5.0-beta'
@@ -22,11 +22,6 @@ Package.registerBuildPlugin({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles(['traceur-runtime.js',
-    'es6-module-loader@0.16.5.js',
-    'system@0.16.7.js'], 'client');
-
-  api.export('define');
 });
 
 Package.onTest(function(api) {
--- packages/angular-ts/plugin/handler.js
+++ packages/angular-ts/plugin/handler.js
@@ -1,23 +1,10 @@
-Plugin.registerSourceHandler('tpl', {
-  isTemplate: true,
-  archMatching: "web"
-}, function(compileStep) {
-  var contents = compileStep.read().toString('utf8');
-
-  compileStep.addAsset({
-    path : compileStep.inputPath,
-    data : contents
-  });
-});
-
 var typescript = Npm.require('typescript');
 
-Plugin.registerSourceHandler('ts.js', function(compileStep) {
+Plugin.registerSourceHandler('ts', function(compileStep) {
   var output = typescript.transpile(compileStep.read().toString('utf8'), { module : typescript.ModuleKind.AMD });
 
   compileStep.addAsset({
-    path : compileStep.inputPath.replace('.ts.js', '.js'),
-    //sourcePath : compileStep.inputPath,
+    path : compileStep.inputPath.replace('.ts', '.js'),
     data : output
   });
 });
