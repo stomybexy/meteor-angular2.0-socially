9dc144b55dac89ca8b6a4f788471b3cb1e663c66
barbatus <alex.borodach@gmail.com>
2015-10-17 14:58:32 +0300

    Step 4.1:  create PartiesForm component

---
+++ client/parties-form/parties-form.ts
@@ -0,0 +1,9 @@
+import {Component, View} from 'angular2/core';
+
+@Component({
+    selector: 'parties-form'
+})
+@View({
+    templateUrl: '/client/parties-form/parties-form.html'
+})
+export class PartiesForm { }
