3ba01138e3364d440e3c6912d8bdb3ea0b519266
barbatus <alex.borodach@gmail.com>
2015-10-17 18:34:57 +0300

    Step 4.6: add form validation

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -14,9 +14,9 @@ export class PartiesForm {
     constructor() {
         var fb = new FormBuilder();
         this.partiesForm = fb.group({
-            name: [''],
+            name: ['', Validators.required],
             description: [''],
-            location: ['']
+            location: ['', Validators.required]
         });
     }
 }
