8220ab9702eef02f77d2d9ecba1ebaaabe2a0d2b
barbatus <alex.borodach@gmail.com>
2015-10-17 17:41:26 +0300

    Step 4.5: build a form model

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -1,9 +1,22 @@
 import {Component, View} from 'angular2/core';
 
+import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
+
 @Component({
     selector: 'parties-form'
 })
 @View({
     templateUrl: '/client/parties-form/parties-form.html'
 })
-export class PartiesForm { }
+export class PartiesForm {
+    partiesForm: ControlGroup;
+
+    constructor() {
+        var fb = new FormBuilder();
+        this.partiesForm = fb.group({
+            name: [''],
+            description: [''],
+            location: ['']
+        });
+    }
+}
