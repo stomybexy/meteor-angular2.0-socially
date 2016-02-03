4b5e01f90d44267e9d175089e15928b44482e6ca
barbatus <alex.borodach@gmail.com>
2016-02-03 19:08:42 +0700

    Step 22.6: prepare PartyDetails to be used from the browser package

--- packages/socially-browser/client/party-details/party-details.ts
+++ packages/socially-browser/client/party-details/party-details.ts
@@ -2,17 +2,13 @@ import {Component, View} from 'angular2/core';
 
 import {RouteParams} from 'angular2/router';
 
-import {Parties} from 'collections/parties';
-
 import {RouterLink} from 'angular2/router';
 
 import {RequireUser, InjectUser} from 'meteor-accounts';
 
-import {MeteorComponent} from 'angular2-meteor';
-
-import {DisplayName} from 'client/lib/pipes';
-
 import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES, MapMouseEvent} from 'ng2-google-maps/core';
+
+import {DisplayName, PartyDetails as PartyDetailsBase} from 'socially-client';
 
 @Component({
     selector: 'party-details'
@@ -24,7 +20,13 @@ import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES, MapMouseEvent} from 'ng2-google-maps/co
 })
 @RequireUser()
 @InjectUser()
-export class PartyDetails extends MeteorComponent {
+export class PartyDetails extends PartyDetailsBase {
+
+    constructor(params: RouteParams) {
+        var partyId = params.get('partyId');
+        super(partyId);
+    }
+
     mapClicked($event: MapMouseEvent) {
         this.party.location.lat = $event.coords.lat;
         this.party.location.lng = $event.coords.lng;
