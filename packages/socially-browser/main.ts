033b7628c4d721c3373c9924bdbf20687feaed5e
barbatus <alex.borodach@gmail.com>
2016-02-03 19:33:07 +0700

    Step 22.8: export package components

---
+++ packages/socially-browser/main.ts
@@ -0,0 +1,8 @@
+
+export * from './client/login/login';
+
+export * from './client/parties-form/parties-form';
+
+export * from './client/parties-list/parties-list';
+
+export * from './client/party-details/party-details';
