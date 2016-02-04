a6847121b2f9cf728aaa17d5ee6995a037594303
barbatus <alex.borodach@gmail.com>
2016-02-04 22:34:28 +0700

    Add Ionic 2 party form component and its template

---
+++ packages/socially-mobile/client/party-form/party-form.ts
@@ -0,0 +1,8 @@
+import {Page, NavController} from 'ionic/ionic';
+
+import {PartiesForm as PartiesFormBase} from 'socially-client';
+
+@Page({
+    templateUrl: '/packages/socially-mobile/client/party-form/party-form.html'
+})
+export class PartiesForm extends PartiesFormBase {}
