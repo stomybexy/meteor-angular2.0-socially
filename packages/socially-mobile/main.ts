65da2b2f8832ac470121a9f4a9b16f82ea5ca13d
barbatus <alex.borodach@gmail.com>
2016-02-05 14:43:22 +0700

    Add main.ts to define package exports

---
+++ packages/socially-mobile/main.ts
@@ -0,0 +1,8 @@
+
+export * from './client/login/login-page';
+
+export * from './client/party-form/party-form';
+
+export * from './client/parties-list/parties-list';
+
+export * from './client/party-details/party-details';
