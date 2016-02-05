c2355bb4f4b555302879e91be746bb05c56950c2
barbatus <alex.borodach@gmail.com>
2016-02-05 16:58:01 +0700

    Step 22.22: export app.ts

--- packages/socially-browser/main.ts
+++ packages/socially-browser/main.ts
@@ -1,8 +1,2 @@
 
-export * from './client/login/login';
-
-export * from './client/parties-form/parties-form';
-
-export * from './client/parties-list/parties-list';
-
-export * from './client/party-details/party-details';
+export * from './app';
